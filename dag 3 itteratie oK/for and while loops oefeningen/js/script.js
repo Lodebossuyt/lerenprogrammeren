59ee5e2ea44e2113dcbd267dd428dae9d1ed5c5e
Lode Bossuyt <lodebossuyt@hotmail.com>
2021-09-15 12:00:24 +0200

    oefening loop getallen

--- dag 3 itteratie oK/for and while loops oefeningen/js/script.js
+++ dag 3 itteratie oK/for and while loops oefeningen/js/script.js
@@ -3,9 +3,10 @@
 // Voorbeeld: Hoeveel getallen?: 3 Geef getal 1 in: 4 Geef getal 2 in: 3 Geef getal 3 in: 2 De totale som van 3 getallen is 9.
 
 var aantalGetallen = parseInt(prompt("hoeveel getallen wilt u optellen?"));
-var i = 0
-for(i;i < aantalGetallen;i++ ){
-   var getal = parseInt(prompt("geef uw getal"));
-   getal = getal + getal;
+var i = 1
+var resultaat = 0;
+for(i;i <= aantalGetallen;i++ ){
+   var getal = parseInt(prompt("geef uw getal" + i + "in"));
+   resultaat = resultaat + getal;
 }
-console.log(getal);
+console.log("de totale som van" + aantalGetallen + "getallen is" + resultaat);
