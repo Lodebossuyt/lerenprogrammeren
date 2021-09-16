35d1b8208feabce2f91d527ced61d409a2420ebf
Lode Bossuyt <lodebossuyt@hotmail.com>
2021-09-16 12:36:13 +0200

    oefening 9 nog niet af

---
+++ dag 4 oefeningen/oefening 8 for loop/js/script.js
@@ -0,0 +1,5 @@
+var getal = prompt("geef uw getal");
+for(i = 1;i<getal;i*=2){
+
+      console.log(i)
+}
---
+++ dag 4 oefeningen/oefening 9 for loop/js/script.js
@@ -0,0 +1,6 @@
+var getal = parseInt(prompt("getal ingeven"));
+for(i = 1;i<getal;i++){
+if(i % x === 0){
+    console.log(i);
+}
+}
