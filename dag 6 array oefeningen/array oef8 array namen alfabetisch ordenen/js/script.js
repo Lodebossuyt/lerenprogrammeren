61841031b281257daefca960ea8dcae73cfe0e9b
Lode Bossuyt <lodebossuyt@hotmail.com>
2021-09-20 14:01:19 +0200

    oefeningen arrays klaar

--- dag 6 array oefeningen/array oef8 array namen alfabetisch ordenen/js/script.js
+++ dag 6 array oefeningen/array oef8 array namen alfabetisch ordenen/js/script.js
@@ -1,34 +1,11 @@
 var eersteArray = [];
-var tweedeArray = [];
-var derdeArray = [];
-var max = parseInt(prompt("geef het maximum getal"));
 
-for(var i = 0; i <10; i++){
-    var randomNumber = Math.floor(Math.random()* max);
-    eersteArray.push(randomNumber);
+var hoeveelNamen = parseInt(prompt("hoeveel namen wilt u ordenen?"));
 
+for(var i = 0; i < hoeveelNamen;i++){
+    var naam = prompt("vul een naam in");
+    eersteArray.push(naam);
 }
-for(var i = 0; i <10; i++){
-    var randomNumber = Math.floor(Math.random()* max);
-    tweedeArray.push(randomNumber);
-
-}
-
-for(var i = 0; i<10;i++){
-
-    if(eersteArray[i] % 2 === 0){
-        derdeArray.push(eersteArray[i]);
-    }
-
-}
-
-for(var i = 0; i<10;i++){
-
-    if(tweedeArray[i] % 2 === 0){
-        derdeArray.push(tweedeArray[i]);
-    }
-
-}
+eersteArray.sort();
 console.log(eersteArray);
-console.log(tweedeArray);
-console.log(derdeArray);
+
